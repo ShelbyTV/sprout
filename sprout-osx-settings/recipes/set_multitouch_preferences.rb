d584e0e74debe2980732f3a7d563843c623fc72f
Dan Spinosa <dan@shelby.tv>
2013-11-22 15:53:34 -0500

    conditionalize multitouch preferences

--- sprout-osx-settings/recipes/set_multitouch_preferences.rb
+++ sprout-osx-settings/recipes/set_multitouch_preferences.rb
@@ -2,16 +2,19 @@ osx_defaults "allow clicking by touch" do
   domain 'com.apple.driver.AppleBluetoothMultitouch.trackpad'
   key 'Clicking'
   integer 1
+  only_if { node['trackpad']['touch_click'] }
 end
 
 osx_defaults "allow dragging by touch" do
   domain 'com.apple.driver.AppleBluetoothMultitouch.trackpad'
   key 'Dragging'
   integer 1
+  only_if { node['trackpad']['touch_drag'] }
 end
 
 osx_defaults "disable swipe between pages" do
   domain 'NSGlobalDomain'
   key 'AppleEnableSwipeNavigateWithScrolls'
   integer 0
+  only_if { node['trackpad']['disable_swipe_between_pages'] }
 end
