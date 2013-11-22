03ffc3d155aee1b6eb3988e6fefcf67d665aabda
Dan Spinosa <dan@shelby.tv>
2013-11-22 14:46:23 -0500

    setting to change default terminal shell

--- sprout-osx-settings/recipes/terminal_preferences.rb
+++ sprout-osx-settings/recipes/terminal_preferences.rb
@@ -9,3 +9,10 @@ osx_defaults "Set startup terminal color scheme to #{node['terminal']['color_sch
   key 'Startup Window Settings'
   string node['terminal']['color_scheme']
 end
+
+osx_defaults "Change the default shell to #{node['terminal']['shell']}" do
+  domain 'com.apple.Terminal'
+  key 'Shell'
+  string node['terminal']['shell']
+  only_if { node['terminal']['shell'] }
+end
